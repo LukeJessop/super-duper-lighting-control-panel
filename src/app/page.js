050ae8047bcf0f29741f018f023bd23522cf1d0f
LukeJessop <70823991+LukeJessop@users.noreply.github.com>
2024-04-28 13:12:35 -0600

    created data structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,34 @@
-import data from '@/data/data.json'
-console.log(data)
+"use client";
 
-const gridSquares = [
-  "Config",
-  ...data.scenes
-]
+import data from "@/data/data.json";
+import { useState } from "react";
+import PowerButton from "../Assets/SVG/power-off-solid.svg";
+import PowerButtonGreen from "../Assets/SVG/power-off-solid-green.svg";
+import Image from "next/image";
+import { Axios } from "axios";
+
+// const gridSquares = ["Config", ...data.scenes];
 
 export default function Home() {
-  
+  const [lightsOn, setLightsOn] = useState(false);
+
+  const toggleLightsRequest = () => {
+    
+  }
+
   return (
     <main className="flex ">
-      {
-        gridSquares.map((item, index) => {
-          return(
-            <div key={index}>
-              {/* {item} */}asdf
-            </div>
-          )
-        })
-      }
+      <button onClick={() => setLightsOn(!lightsOn)}>
+        <div className="p-4">
+          <div className="w-12 h-12">
+            {lightsOn ? (
+              <Image alt="power-button" src={PowerButtonGreen} />
+            ) : (
+              <Image alt="power-button" src={PowerButton} />
+            )}
+          </div>
+        </div>
+      </button>
     </main>
-  )
+  );
 }
-
-
-
